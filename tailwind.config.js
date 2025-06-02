66924aa5e1bb014c2419934a17e7384fb5092aa1
YCN <89133149+sifyacine@users.noreply.github.com>
2025-06-03 00:13:24 +0100

    set up

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class", // Enables dark mode via a class
+  plugins: [],
+};
