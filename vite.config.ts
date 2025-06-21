014fb2f263a12fddfc8cf984379e53e92a639dea
YCN <89133149+sifyacine@users.noreply.github.com>
2025-06-21 10:39:39 +0100

    remove dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Deploying to root of pedaconnect.com
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
